6a674d60ce12f46d017091002b1d8b1b26697553
Parv <parv@gmail.com>
2026-02-17 21:26:59 +0530

    Added gunicorn and requirements

--- garage/settings.py
+++ garage/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # -------------------------------------------------
 SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key")
 
-DEBUG = os.environ.get("DEBUG", "True") == "True"
+DEBUG = os.environ.get("DEBUG", "True") == "False"
 
 ALLOWED_HOSTS = ['*']  # Replace with domain in production
 
